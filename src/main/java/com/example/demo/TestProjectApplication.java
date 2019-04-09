5bbcf0aae0a8babc4c3c1c99b3ebcfb9687a2893
CHAAND <CHAAND@CHAAND-PC>
2019-04-10 00:31:29 +0530

    feature3 added

--- src/main/java/com/example/demo/TestProjectApplication.java
+++ src/main/java/com/example/demo/TestProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Test feature");
+		System.out.println("Test feature3");
 		SpringApplication.run(TestProjectApplication.class, args);
 	}
 
